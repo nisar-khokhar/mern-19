30ea27e49f928bb3ef1e667adeb7c095e3d39b6a
nisar-khokhar <khokharmaliknisar@gmail.com>
2024-07-22 13:36:26 +0500

    added a template string

--- primitiveDatatypes.js
+++ primitiveDatatypes.js
@@ -13,3 +13,10 @@ const f = 78234526576237945645645645154564239234729;
 console.log(typeof f);
 
 //another primitive datatype is symbol
+
+//Template strings
+const name1 = "Ali";
+const name2 = "Hassan";
+
+const friends = `${name1} and ${name2} are the friends of Hussain.`;
+console.log(friends);
