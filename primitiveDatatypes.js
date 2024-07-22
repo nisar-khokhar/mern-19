5317db52efc51fa518c37518da09f2d442eb350e
nisar-khokhar <khokharmaliknisar@gmail.com>
2024-07-22 13:30:45 +0500

    primitive dataTypes file been created

---
+++ primitiveDatatypes.js
@@ -0,0 +1,15 @@
+//number
+const a = 5;
+//undefined
+let b;
+//string
+const c = "Hello World";
+//boolean
+const d = false;
+//null
+const e = null;
+//bigInt
+const f = 78234526576237945645645645154564239234729;
+console.log(typeof f);
+
+//another primitive datatype is symbol
