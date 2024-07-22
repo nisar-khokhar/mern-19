cb2415bd62f4520420c01d44ec664ea5def2c27a
nisar-khokhar <khokharmaliknisar@gmail.com>
2024-07-22 13:05:14 +0500

    practised different operators

---
+++ operators.js
@@ -0,0 +1,19 @@
+// binary operators
+
+console.log(1 + 2);
+console.log(2 - 1);
+console.log(2 * 1);
+console.log(10 / 5);
+console.log(2 ** 10);
+
+//logical operators
+console.log(5 > 1);
+console.log(2 < 1);
+console.log(2 == 2);
+console.log(1 != 2);
+
+//bitwise operators
+console.log(1 | 2);
+console.log(1 & 2);
+console.log(5 ^ 3);
+console.log(~5);
